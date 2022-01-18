a220391640f9437954e06e5e0be1a0273fefe6bc
Siggi <4411176+icellan@users.noreply.github.com>
2022-01-18 22:54:52 +0100

    Bumped to 2.0.0

--- package.js
+++ package.js
@@ -2,16 +2,16 @@ Package.describe({
   name: "icellan:moneybutton-oauth",
   summary: "Money Button OAuth flow",
   git: "https://github.com/icellan/meteor-moneybutton-oauth.git",
-  version: "1.0.2"
+  version: "2.0.0"
 });
 
 Package.onUse(function(api) {
-  api.use('ecmascript@0.12.7');
+  api.use('ecmascript');
   api.use('oauth@1.2.8', ['client', 'server']);
   api.use('oauth2@1.2.1', ['client', 'server']);
-  api.use('random@1.1.0', 'client');
-  api.use('underscore@1.0.10', 'server');
-  api.use('service-configuration@1.0.11', ['client', 'server']);
+  api.use('random', 'client');
+  api.use('underscore', 'server');
+  api.use('service-configuration', ['client', 'server']);
 
   api.addFiles('moneybutton_common.js', ['server', 'client']);
 
